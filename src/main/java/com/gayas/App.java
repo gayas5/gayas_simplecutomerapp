3cf83f3b65b09b1b7ca984de7033bb27e759ed3b
gayas5 <gayasuddin541999@gmail.com>
2026-01-22 17:26:23 +0530

    Create App.java

---
+++ src/main/java/com/gayas/App.java
@@ -0,0 +1,12 @@
+package com.gayas;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gayas Customer App!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
